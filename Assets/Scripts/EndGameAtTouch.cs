7b21ac8a30b786e3fa0158d6f3f931ac50890494
Tarlan Isaev <tamerlanium@gmail.com>
2018-08-30 16:09:14 +0300

    First commit

---
+++ Assets/Scripts/EndGameAtTouch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameAtTouch : MonoBehaviour {
+
+	public bool endWithWin;
+
+	void OnTriggerExit2D(Collider2D target) {
+		if (target.gameObject.tag == "Player") {
+
+			if (endWithWin == true) {
+				PersistentManager.dataStore.endGameWithWin ();
+			} else {
+				PersistentManager.dataStore.endGameWithLoss ();
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour {
+
+	public Sprite[] sprites;
+
+	void Start () {
+		var myRender = GetComponent<SpriteRenderer> ();
+
+		if (sprites.Length > 0) {
+			myRender.sprite = sprites [Random.Range (0, sprites.Length)];
+			myRender.sortingOrder = Random.Range (1, 11);
+		}
+	}
+}
